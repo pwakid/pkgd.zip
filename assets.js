88d211f26a6f86162874c05240812b19743e087d
Travis McKinkley <163606441+pwakid@users.noreply.github.com>
2024-08-08 08:41:49 -0400

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let keySequence = [];
     const hotkeySequence = ['x', 'z'];
