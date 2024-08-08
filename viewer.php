da8b3ab7e34e5a82dc01fe737828343390ea632e
Travis McKinkley <163606441+pwakid@users.noreply.github.com>
2024-08-08 07:37:45 -0400

    Create viewer.php

---
+++ viewer.php
@@ -0,0 +1,27 @@
+<?php if(isset($_GET['f'])) { $f = trim($_GET['f']); } else { die("404"); } ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>File Viewer</title>
+    <style>
+        #fileContent {
+            width: 100%;
+            min-height: 800px;
+        }
+    </style>
+</head>
+<body>
+    <label for="fileSelect">Choose a file:</label>
+    <select id="fileSelect" onchange="loadFileContent(this.value)">
+        <option value="">--Select a file--</option>
+        <option value="a.txt">a.txt</option>
+        <option value="asd.html"s>asd.html</option>
+        <option value="image.php">image.php</option>
+    </select>
+    <br><br>
+    <textarea id="fileContent"></textarea>
+    <script src="assets/files.js"></script>
+</body>
+</html>
