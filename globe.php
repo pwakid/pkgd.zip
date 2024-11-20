a70ff819057725a8e32ba1222eb60e1d4f54ad70
Travis McKinkley <163606441+pwakid@users.noreply.github.com>
2024-11-20 11:46:40 -0500

    Update globe.php

--- globe.php
+++ globe.php
@@ -1,4 +1,29 @@
 <?php
+
+function timeAgo($timestamp) {
+    $time = time() - strtotime($timestamp);
+
+    $units = [
+        "year"   => 365 * 24 * 60 * 60,
+        "month"  => 30 * 24 * 60 * 60,
+        "week"   => 7 * 24 * 60 * 60,
+        "day"    => 24 * 60 * 60,
+        "hour"   => 60 * 60,
+        "minute" => 60,
+        "second" => 1
+    ];
+
+    foreach ($units as $name => $duration) {
+        if ($time >= $duration) {
+            $count = floor($time / $duration);
+            return "$count $name" . ($count > 1 ? "s" : "") . " ago";
+        }
+    }
+
+    return "Just now";
+}
+
+
 $basedir = __DIR__;
 $dir = $_GET['dir'];
 $file = $_GET['file'];
